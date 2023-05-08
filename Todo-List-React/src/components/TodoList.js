aeed718809205edd99de8f1a86ceda1667e9cfb7
kmr-sahil <91898837+kmr-sahil@users.noreply.github.com>
2023-05-08 16:44:53 +0530

    Update TodoList.js

--- Todo-List-React/src/components/TodoList.js
+++ Todo-List-React/src/components/TodoList.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import styles from "./style.module.css"
 
+
 export default function TodoList(props) {
+  
   const handleDelete = () => {
-    props.setTodoList((props.todoList).filter((item) => item.id !==  props.todoItem.id))
+    props.setDone(prev => prev + 1)
+    props.setTodoList((props.todoList).filter((item) => item.id !== props.todoItem.id))
+
   }
+
   return (
-    <div>
-      <div className={styles.todoitem}>
+    <div className={styles.todoitem}>
+    
         <h3 className={styles.task}>{props.todoItem.name}</h3>
         <button onClick={handleDelete} className={styles.doneBtn}>Done</button>
-      </div>
-        
+    
     </div>
   )
 }
